163601fae3293ddc45b64fa41ae5e538d907560c
Guoyuan Liu <liuguoyuan01@outlook.com>
2023-09-13 21:01:05 +0200

    add ModelOptimizer

--- src/Systems/model.jl
+++ src/Systems/model.jl
@@ -1,4 +1,6 @@
 using MCMCChains, Turing, Distributions, StatsBase, Combinatorics
+using Optim
+using Optim: minimizer, LineSearches 
 
 function E_hund_exchange(diag_occs, J)
     # -J_H Σ (1/2 + S_α * S_β)
@@ -73,6 +75,21 @@ function prepare_data(l::Searcher)
     return (; magmoms_squared_sum, diag_occs, E_Us, energies, states, diag_occupations_sum, diag_occupations_squared_sum)
 end
 
+# TODO duplicate code
+function prepare_data(occs, U)
+    magmoms = map(occupations) do occupation
+        map(occupation) do occ
+            tr(view(occ, Up())) - tr(view(occ, Down())) 
+        end
+    end
+    magmoms_squared_sum = map(magmom -> sum(m-> m^2, magmom), magmoms)
+    E_Us = map(occ-> sum(m -> E_U(view(m, Up()), U) + E_U(view(m, Down()), U), occ), occupations)
+    diag_occs = map(occs->map(x->hcat(diag(view(x, Up())), diag(view(x, Down()))), occs), occupations)
+    diag_occupations_sum = map(occ -> sum(x-> (diag(view(x, Up())) .+ diag(view(x, Down()))), occ), occupations)
+    diag_occupations_squared_sum = [d.^2 for d in diag_occupations_sum]
+    return (; magmoms_squared_sum, diag_occupations_sum, E_Us, diag_occupations_squared_sum, diag_occs)
+end
+
 @model function total_energy(data, ::Type{T}=Float64) where {T}
     # physics
     α ~ truncated(Normal(0, 2); lower = 0)
@@ -105,6 +122,9 @@ end
 end
 
 struct ModelTrainer <: System end
+function Overseer.requested_components(::ModelTrainer)
+    return (TrainerSettings, Intersection, Model)
+end
 
 function Overseer.update(::ModelTrainer, m::AbstractLedger)
 
@@ -133,15 +153,133 @@ function Overseer.update(::ModelTrainer, m::AbstractLedger)
     end
 end
 
+function occ2x(occ)
+    eigen_vals = Float64[]
+    angles = Float64[]
+    up=view(occ, Up())
+    down = view(occ, Down())
+    for m in [up, down]
+        vals, vecs = eigen(m)
+        agl = Angles(vecs) 
+        append!(eigen_vals, vals)
+        append!(angles, agl.flat)
+    end
+    return vcat(eigen_vals, angles)
+end
+
+function x2occ(x)
+    vals = x[1:10]
+    up_angles = Angles(x[11:20])
+    down_angles = Angles(x[21:30])
+    up_vecs = Matrix(up_angles)
+    down_vecs = Matrix(down_angles)
+    up_occ = Matrix(Eigen(vals[1:5], up_vecs))
+    down_occ = Matrix(Eigen(vals[6:10], down_vecs))
+    occs = ColinMatrix(hcat(up_occ, down_occ))
+end
+
+function model_diag(model, func, U, x; use_penalty=false)
+    @assert size(x, 1) == 30
+    occs = [x2occ(x)] # for now we have one atom
+    data = prepare_data([occs], U)
+    ys = func(model.α, model.Jh, model.C, model.constant_shift, data, Float64)
+    if use_penalty
+        eigenvals = x[1:10]
+        penalty =  1e2 * sum(@. (max(0, eigenvals-1) + max(0, -eigenvals)))
+        ys += penalty
+    end
+    return ys
+end
+
+function optimize_cg(x0, model_diag, lower, upper)
+    # conjugate gradient
+    res = optimize(
+        model_diag,
+        lower, upper, x0,
+        Fminbox(ConjugateGradient(; linesearch= LineSearches.BackTracking())),
+        Optim.Options(iterations=100, outer_iterations=10, show_trace=true))
+    x_min = minimizer(res);
+    return x2occ(x_min);
+end
+
+function optimize_sa(x0, model_diag, lower, upper)
+    # simulated annearling with bounds
+    res = optimize(
+        model_diag,
+        lower, upper, x0,
+        SAMIN(
+            rt=0.95, # temperature reduction rate controls space covered, higher -> more coverage
+            verbosity=1,
+        ), 
+        Optim.Options(iterations=10^6, show_trace=true)
+    )
+    x_min = minimizer(res);
+    return x2occ(x_min);
+end
+
+function optimize_sa(x0, model_diag, lower, upper)
+# @pooled_component Base.@kwdef struct OptimizeSettings
+#     opts::Vector{Optimizer}
+#     opt_options::Vector{Dict}
+# end
+
 struct ModelOptimizer <: System end
+function Overseer.requested_components(::ModelOptimizer)
+    return (TrainerSettings, Intersection, Model)
+end
     
 function Overseer.update(::ModelOptimizer, m::AbstractLedger)
-    # check how many calcs are pending
-    # if free room -> generate some more calcs with the current model
-    # check Unique + Results and Trial whether it's duplication on either
-    # Generate Trial with origin ModelOptimized + increment generation
+    # if no model yet, skip
+    model = isempty(m[Model]) ? nothing : m[Model][end]
+    model === nothing && return
+    U = DFC.getfirst(x->x.dftu.U != 0, m[Template][1].structure.atoms).dftu.U
+    model_diag(x) = model_diag(model, on_site_energy, U, x)
 
+    # check how many calcs are pending on server
+    # if free room -> generate some more calcs with the current model
+    n_current_simjobs   = length(@entities_in(m, SimJob && !Error))
+    max_concurrent_jobs = sum(x -> Server(x.server).max_concurrent_jobs, m[ServerInfo])
+    max_new             = max_concurrent_jobs - n_current_simjobs
+    max_new <= 0 && return 
+ 
+    # find previous unique, intersection, other trials about to run
+    # to check whether it's duplication
+    unique_states = @entities_in(m, (Unique || Intersection) && Results)
+    pending_states = @entities_in(m, SimJob && Submit)
+
+    # Generate Trial with origin ModelOptimized + increment generation
     # Severely limit the amount of new intersections, maybe only 6 per generation or whatever
     #  new trial -> hopefully new unique -> 3 x whatever prev unique intersections -> train model whenever Y new unique states are found from those -> rinse repeat 
+    lower = vcat(ones(10) * (-0.0), ones(20) * (-π-1e-3))
+    upper = vcat(ones(10) * 1.0, ones(20) * (π +1e-3))
+
+    strc = m[BaseCase][1].structure
+    natoms = length(filter(ismagnetic, str.atoms))
+    nshell = size(m[BaseCase][Results].state.occupations[1], 1)
+    dist_thr = 1e-2
+    curgen = maximum_generation(m)
+    # TODO multithreading
+    while max_new > 0
+        # random start
+        x0 = rand(natoms * nshell) # TODO slightly better random?
+        occ = optimize_cg(x0, model_diag, lower, upper)
+        state0 = State(occ)
+        distances = map(e->Euclidean()(e[Results].state, state0), unique_states)
+                  ∪ map(e->Euclidean()(e[Trial].state, state0), pending_states)
+        if minimum(distances; init=Inf) < dist_thr
+            continue
+        end
+        trial = Trial(state0, RomeoDFT.ModelOptimized) # TODO other tag?
+        calc = deepcopy(m[BaseCase][1].calculation)
+        calc[:system][:Hubbard_occupations] = generate_Hubbard_occupations(trial.state, strc)
+        gen = Generation(curgen + 1) # TODO this is wrong
+
+        # add new entity with optimized occ
+        new_e = Entity(m, trial, calc, gen) 
+        max_new -= 1
+    end
+
 end
+
+
 
